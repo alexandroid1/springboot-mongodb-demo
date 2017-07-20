d49787a8f991fef0d9d2ccca530a4c40c4d924b2
Alexksandr <archangel7017@gmail.com>
2017-07-20 11:41:03 +0300

    properties

---
+++ src/main/java/rc/HotelController.java
@@ -0,0 +1,24 @@
+package rc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Александр on 20.07.2017.
+ */
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+    private HotelRepository hotelRepository;
+
+    @GetMapping("/all")
+    public List<Hotel> getAll(){
+        List<Hotel> hotels = this.hotelRepository.findAll();
+
+        return  hotels;
+    }
+}
