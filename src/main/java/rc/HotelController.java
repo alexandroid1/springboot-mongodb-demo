af29674be9e74dae55065aed508bb2bb8a7b8170
Alexksandr <archangel7017@gmail.com>
2017-07-24 12:31:46 +0300

    filter by max Price

--- src/main/java/rc/HotelController.java
+++ src/main/java/rc/HotelController.java
@@ -1,8 +1,6 @@
 package rc;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +20,35 @@ public class HotelController {
     @GetMapping("/all")
     public List<Hotel> getAll(){
         List<Hotel> hotels = this.hotelRepository.findAll();
-
         return  hotels;
+    }
+
+    @PutMapping
+    public void insert(@RequestBody Hotel hotel){
+        this.hotelRepository.insert(hotel);
+    }
+
+    @PostMapping
+    public void update(@RequestBody Hotel hotel){
+        this.hotelRepository.save(hotel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        this.hotelRepository.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Hotel getById(@PathVariable("id")String id){
+        Hotel hotel = this.hotelRepository.findById(id);
+
+        return hotel;
+    }
+
+    @GetMapping("/price/{maxPrice}")
+    public List<Hotel> getByPricePerNight(@PathVariable("maxPrice") int maxPrice){
+        List<Hotel> hotels = this.hotelRepository.findByPricePerNightLessThan(maxPrice);
+
+        return hotels;
     }
 }
--- src/main/java/rc/HotelRepository.java
+++ src/main/java/rc/HotelRepository.java
@@ -3,10 +3,14 @@ package rc;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by Александр on 19.07.2017.
  */
 
 @Repository
 public interface HotelRepository extends MongoRepository<Hotel,String> {
+    Hotel findById(String id);
+    List<Hotel> findByPricePerNightLessThan(int maxPrice);
 }
